c8bdb45697d1635cac2ee4b5334474ec78de00bb
farleybsd <farley.t.i@hotmail.com>
2023-06-30 16:56:28 -0300

    Fundamentos

---
+++ Capitulo1/Aula01/fundamentos/src/App.jsx
@@ -0,0 +1,19 @@
+import {Post} from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post 
+      author ="Farley Rufino"  
+      content ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus distinctio, culpa maiores harum quis dolorum fuga vero voluptatem nemo ipsam commodi laudantium dignissimos ullam molestiae magnam alias officiis illo asperiores?"
+      />
+     
+      <Post 
+      author ="Thalitinha"  
+      content ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus distinctio, culpa maiores harum quis dolorum fuga vero voluptatem nemo ipsam commodi laudantium dignissimos ullam molestiae magnam alias officiis illo asperiores?"
+      />
+
+    </div>
+  )
+}
+
